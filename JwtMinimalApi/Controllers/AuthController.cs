9329f261005bc6f3a81f44819f4abb5383c7a272
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-26 18:32:26 +0300

    add AuthController that will handle the requests and responses

---
+++ JwtMinimalApi/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using JwtMinimalApi.Models;
+using JwtMinimalApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtMinimalApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly JwtTokenGenerator _tokenGenerator;
+
+    public AuthController(JwtTokenGenerator tokenGenerator)
+    {
+        _tokenGenerator = tokenGenerator;
+    }
+
+
+    [HttpPost("login")]
+    public IActionResult Login(User requestUser)
+    {
+        var token = _tokenGenerator.GenerateToken(requestUser);
+
+        if (token == null)
+            return Unauthorized("Invalid username or password");
+
+        return Ok(new { token });
+    }
+
+
+    [Authorize]
+    [HttpGet("welcome")]
+    public IActionResult GetSecret()
+    {
+        return Ok($"Welcome!!!");
+    }
+}
