55591f53db602bb29eb6bbe4c0a14c7c5625c513
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-26 17:58:37 +0300

    add UserService to store list of users

---
+++ JwtMinimalApi/Services/UserService.cs
@@ -0,0 +1,12 @@
+﻿using JwtMinimalApi.Models;
+
+namespace JwtMinimalApi.Services;
+
+public class UserService
+{
+    private readonly List<User> users = new List<User>
+    {
+        new User { Id = 1,FirstName = "Marwa", LastName = "AbuSaa", Password = "1230", UserName = "marwaSaa"},
+        new User { Id = 2,FirstName = "Aya", LastName = "Ba'ara", Password = "9870", UserName = "ayaBaara"}
+    };
+}
