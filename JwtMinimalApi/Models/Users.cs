ee1f819d9991dc814d2bd2fe2d7a1ebc19ac0143
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-26 17:47:11 +0300

    add Users class

---
+++ JwtMinimalApi/Models/Users.cs
@@ -0,0 +1,10 @@
+﻿namespace JwtMinimalApi.Models;
+
+public class Users
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? UserName { get; set; }
+    public string? Password { get; set; }
+}
